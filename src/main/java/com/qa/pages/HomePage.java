72d3452530f4a2237dad3d22aab83cef66ec6173
Neha Mandal <nehamandal@Nehas-MacBook-Air.local>
2021-04-10 21:46:36 +0530

    Second git commit

--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -2,4 +2,13 @@ package com.qa.pages;
 
 public class HomePage {
 
+	public void homepage() {
+		
+		System.out.println("Home Page");
+	}
+	
+public void home() {
+		
+		System.out.println("Home ");
+	}
 }
---
+++ src/main/java/com/qa/util/Logout.java
@@ -0,0 +1,12 @@
+package com.qa.util;
+
+import org.testng.annotations.Test;
+
+public class Logout {
+	
+	@Test
+	public void logout() {
+		System.out.println("Logout");
+	}
+
+}
